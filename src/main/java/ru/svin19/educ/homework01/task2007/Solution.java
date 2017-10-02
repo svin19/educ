3e86a951bfbf4cb5ff959eebebacab9b3cce4ccb
svin19 <svin1250229@gmail.com>
2017-10-02 23:27:26 +0300

    Task2007

---
+++ src/main/java/ru/svin19/educ/homework01/task2007/Solution.java
@@ -0,0 +1,17 @@
+package ru.svin19.educ.homework01.task2007;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        for (int i = 1; ; i++) {
+            if (value % 2 != 0) {
+                System.out.println(i - 1);
+                return;
+            }
+            value /= 2;
+        }
+    }
+}
